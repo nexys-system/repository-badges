5be715422bebaa0d4312916960dcdcae92c62607
johnb8005 <johnb8005@users.noreply.github.com>
2021-09-18 11:25:38 +0000

    deploy: 4446d2c49cae1b251936bd47d4741a5f7581c69a

--- _snowpack/env.js
+++ _snowpack/env.js
@@ -1,4 +1,5 @@
-export const SNOWPACK_PUBLIC_GIT_SHA = "3930b8e8bf742207968cd31c5a4fe4f7efb2c66e";
+export const SNOWPACK_PUBLIC_GIT_SHA = "4446d2c49cae1b251936bd47d4741a5f7581c69a";
+export const SNOWPACK_PUBLIC_VERSION = "v0.0.1";
 export const SNOWPACK_PUBLIC_URL = "/repository-badges";
 export const MODE = "development";
 export const NODE_ENV = "development";
--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 import * as __SNOWPACK_ENV__ from './_snowpack/env.js';
 
-export const sha = __SNOWPACK_ENV__.SNOWPACK_PUBLIC_GIT_SHA || "arandomsha";
+export const version = __SNOWPACK_ENV__.SNOWPACK_PUBLIC_VERSION || "unsetversion";
+export const sha = __SNOWPACK_ENV__.SNOWPACK_PUBLIC_GIT_SHA || "unsetsha";
 export const title = __SNOWPACK_ENV__.SNOWPACK_PUBLIC_TITLE || "Repository Badges";
 export const basename = __SNOWPACK_ENV__.SNOWPACK_PUBLIC_URL;
 const ghUrl = "https://github.com/nexys-system/repository-badges";
