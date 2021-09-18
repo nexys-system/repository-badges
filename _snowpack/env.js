d87f500edbfa237daaa2aaf370e452b3ea0698c4
johnb8005 <johnb8005@users.noreply.github.com>
2021-09-18 11:17:46 +0000

    deploy: e1d7b4f5eec52642c9e7db9cf806694fe8fc8454

--- _snowpack/env.js
+++ _snowpack/env.js
@@ -1,4 +1,4 @@
-export const SNOWPACK_PUBLIC_GIT_SHA = "5a8f19d2915d8671e290ff1cedc743527d1f5e96";
+export const SNOWPACK_PUBLIC_GIT_SHA = "e1d7b4f5eec52642c9e7db9cf806694fe8fc8454";
 export const SNOWPACK_PUBLIC_URL = "/repository-badges";
 export const MODE = "development";
 export const NODE_ENV = "development";
